a8e99315bea8d48728f24428c72b5ab84fed0dff
Dune Desormeaux <dune.desormeaux@gmail.com>
2026-03-28 12:27:34 -0700

    license

--- monome_protocol.h
+++ monome_protocol.h
@@ -40,7 +40,6 @@
  *   provided buffer and return the number of bytes written.
  *   Use whichever functions match your role.
  *
- * MIT License — see bottom of file.
  */
 
 #ifndef MONOME_PROTOCOL_H
@@ -1300,27 +1299,3 @@ void monome_arc_state_apply(monome_arc_state_t *a,
 
 #endif /* MONOME_PROTOCOL_IMPLEMENTATION_GUARD */
 #endif /* MONOME_PROTOCOL_IMPLEMENTATION */
-
-/*
- * MIT License
- *
- * Copyright (c) 2026 monome
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
